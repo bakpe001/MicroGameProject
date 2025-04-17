b4fbba08e9dfcf52c0a896a643c5e48230780d67
William <gwido001@edu.xamk.fi>
2025-04-17 11:38:25 +0300

    Added spawn Manager code

--- Prototype 3 Assignment Ver/Assets/Scripts/GameManager.cs
+++ Prototype 3 Assignment Ver/Assets/Scripts/GameManager.cs
@@ -7,10 +7,18 @@ public class GameManager : MonoBehaviour
     public float speed = 15f; //Speed of the obsticles and backgound
     public bool gameOver = false;
 
+    //Refrences to every model, texture and backgound avaiable in the game
     public Background backgound; //Static Refrence to the backgound element
     public PlayerModel playerModel; //Static Refrence to the playerModel element
     public SkinOptions[] skinOptions; //Static Refrence array to the all the skin options for each playermodel element
     private int currentSkin = 0; //Current Texture selected
+
+    //Modifiers of spawnning obsticles
+    public float startDelay= 2f; //Delay before the game starts spawning obsticles
+    public float repeatDelay = 2f; //Delay between the game spawning obsticles
+    public bool spawningOn = true; //Bool to determin if the game is spawning obsticles
+    public SpawnManager spawnManager; //Reference to the SpawnManager script
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -39,6 +47,16 @@ public class GameManager : MonoBehaviour
         {
             // Move to the next child (looping)
             skinOptions[currentSkin].ChangeTexture(); // Change the texture
+        }
+        if (Input.GetKeyDown(KeyCode.N)) // Detect when K is pressed
+        {
+            spawningOn = false; // Stop spawning obsticles
+            spawnManager.spawnReady = false;
+        }
+        if (Input.GetKeyDown(KeyCode.B)) // Detect when K is pressed
+        {
+            spawningOn = true; // Stop spawning obsticles
+            spawnManager.StartCoroutine(spawnManager.SpawnDelay(startDelay)); // Start spawning obsticles
         }
     }
 
--- Prototype 3 Assignment Ver/Assets/Scripts/SpawnManager.cs
+++ Prototype 3 Assignment Ver/Assets/Scripts/SpawnManager.cs
@@ -1,26 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstacles; //Array of all the possible game obsticles
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public GameManager gameManager; //Reference to the GameManager script
+    private Vector3 spawnPos = new Vector3(25, 1, 0);
+    public bool spawnReady = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Temp way of spawning obsticles
-        InvokeRepeating("SpawnObsticle", 1f, 2f);
+        StartCoroutine(SpawnDelay(gameManager.startDelay)); //Start the spawn delay
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.spawningOn)
+        {
+            SpawnObsticle();
+        }
     }
 
     void SpawnObsticle()
     {
-        //Temp Spawning solution (doesnt stop on death)
-        Instantiate(obstacles[0], spawnPos, obstacles[0].transform.rotation);
+        if (!gameManager.gameOver && spawnReady)
+        {
+            //Randomly select an obsticle from the array
+            int obsticleIndex = Random.Range(0, obstacles.Length);
+            Instantiate(obstacles[obsticleIndex], spawnPos, obstacles[obsticleIndex].transform.rotation);
+            spawnReady = false;
+            StartCoroutine(SpawnDelay(gameManager.repeatDelay));
+        }
+    }
+    public IEnumerator SpawnDelay(float waitTime) 
+    { 
+        yield return new WaitForSeconds(waitTime);
+        spawnReady = true;
+        if (!gameManager.spawningOn)
+        {
+            spawnReady = false; //Stop spawning obsticles
+        }
     }
 }
